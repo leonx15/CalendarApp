c00201d5fd821002c162ef325db622d0d156c57a
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2023-12-27 19:55:10 +0100

    Update event link apply.

--- app/static/js/events/event_list.js
+++ app/static/js/events/event_list.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
             const listContainer = document.getElementById('events-list');
             events.forEach(event => {
                 const item = document.createElement('div');
-                item.innerHTML = `<strong>${event.name}</strong> - ${event.start_date}, ${event.end_date}`;
+                const editUrl = `/event/update/${event.id}`;
+                item.innerHTML = `<a href="${editUrl}"><strong>${event.name}</strong></a> - ${event.start_date}, ${event.end_date}`;
                 listContainer.appendChild(item);
             });
         })
