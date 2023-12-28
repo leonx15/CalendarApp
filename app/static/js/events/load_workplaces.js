96b0746422044987674a6612d0ba25e57bf42788
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2023-12-28 12:20:09 +0100

    Adjustments to handle selected at the moment workplace in event.

--- app/static/js/events/load_workplaces.js
+++ app/static/js/events/load_workplaces.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var selectedWorkplaceId = window.selectedWorkplaceId || "";
     // Fetch workplaces and populate the dropdown
     fetch('/api/workplaces')
         .then(response => response.json())
@@ -8,6 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (workplace.active) {
                     const option = document.createElement('option');
                     option.value = workplace.id;
+                    if (selectedWorkplaceId && workplace.id == selectedWorkplaceId) {
+                        option.selected = true;
+                    }
                     option.textContent = workplace.name;
                     dropdown.appendChild(option);
                 }
